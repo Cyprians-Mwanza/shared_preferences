d1850c647d23fe9d049111ebe979142496df7488
Cyprians Mwanza <cypriansmwanza@outlook.com>
2025-10-19 15:52:20 +0300

    test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:shared_preferences/main.dart';
+import 'package:the_shared_preferences/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
